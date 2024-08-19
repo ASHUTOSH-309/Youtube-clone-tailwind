14a083f1f8b983e8d6838ef57da65c528272d442
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-19 22:48:18 +0530

    VideoCard Component basic structure laid

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,57 @@
+
+
+export function VideoCard() {
+
+
+
+
+    return <div>
+
+        <img src="/striver.jpg" className="rounded-md" />
+
+        <div className="grid grid-cols-12 pt-2">
+
+            <div className="col-span-1">
+                <img className="rounded-full w-20 h-20" src="/yotube.jpg" />
+            </div>
+
+            <div className="col-span-11 pl-5">
+
+                <div>
+                    BrockLesnar Most dangerous Fights
+                </div>
+
+                <div className="col-span-11 text-gray-600 text-base">
+                        Ashutosh Sharma
+                </div>
+                <div className="col-span-11 text-gray-600 text-base">
+                        13M views | 5 days ago
+                </div>
+            </div>
+
+        </div>
+    </div>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
