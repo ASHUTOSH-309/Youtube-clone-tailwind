ed786cc6b17dffb891e7b48b72ad9d77e85e336c
ASHUTOSH-309 <ashutoh.sharma83020@gmail.com>
2024-08-19 23:07:34 +0530

    VideoCard component made parameterized

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,31 +1,23 @@
 
 
-export function VideoCard() {
-
-
-
+export function VideoCard(props:any) {
 
     return <div>
-
-        <img src="/striver.jpg" className="rounded-md" />
-
+        <img src={props.thumbnail} className="rounded-md" />
         <div className="grid grid-cols-12 pt-2">
-
             <div className="col-span-1">
-                <img className="rounded-full w-20 h-20" src="/yotube.jpg" />
+                <img className="rounded-full w-20 h-20" src={props.Logoimage} />
             </div>
-
             <div className="col-span-11 pl-5">
-
                 <div>
-                    BrockLesnar Most dangerous Fights
+                    {props.title}
                 </div>
 
                 <div className="col-span-11 text-gray-600 text-base">
-                        Ashutosh Sharma
+                       {props.author}
                 </div>
                 <div className="col-span-11 text-gray-600 text-base">
-                        13M views | 5 days ago
+                        {props.views} | {props.timestamp}
                 </div>
             </div>
 
